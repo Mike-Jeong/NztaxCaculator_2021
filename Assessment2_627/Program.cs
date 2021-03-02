8811e793d1d12cee4404fd9e0ca8c358caeb89f7
Mike-Jeong <alstlr65@gmail.com>
2021-03-03 11:39:58 +1300

    초기 커밋.

---
+++ Assessment2_627/Program.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment2_627
+{
+    class User
+    {
+
+        public string username;
+        protected string tax_Code = "Not identified";
+        protected double tax_Payable;
+        protected double income;
+
+        public double Income
+        {
+            get { return income; }
+            set
+            {
+                if (value > 0)
+                {
+                    income = value;
+                }
+                else
+                {
+                    income = 0;
+                }
+            }
+        }
+
+        public string Tax_Code
+        {
+            get { return tax_Code; }
+            set
+            {
+                tax_Code = value;
+
+                if (value != "n")
+                {
+                    tax_Code = value;
+                }
+                else
+                {
+
+                }
+            }
+
+        }
+
+        public double Tax_Payable
+        {
+            get { return tax_Payable; }
+            set
+            {
+                if (value > 0)
+                {
+                    tax_Payable = value;
+                }
+                else
+                {
+                    tax_Payable = 0;
+                }
+            }
+        }
+
+        public void Show_Taxinformation()
+        {
+            Console.WriteLine("Hello {0}", username);
+            Console.WriteLine("Your Tax code is {0}", tax_Code);
+            Console.WriteLine("And, your Tax payable is {0}", tax_Payable);
+            Console.ReadLine();
+
+
+        }
+
+
+    }
+
+    class System_1 : User
+    {
+
+      
+
+    }
+
+    class System_2 : User
+    {
+
+        public double Tax_Caculation(double Income)
+        {
+            if (Income > 0 && Income < 14000)
+            {
+                return Income * 0.105;
+            }
+            else if (Income >= 14000 && Income < 48000)
+            {
+                return Income * 0.175;
+            }
+            else if (Income >= 48000 && Income < 70000)
+            {
+                return Income * 0.3;
+            }
+            else if (Income >= 70000)
+            {
+                return Income * 0.33;
+            }
+
+            else
+            {
+                return 0;
+            }
+
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            User user = new User();
+            Console.WriteLine("Hi there, Could you enter your name?");
+            user.username = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Hi {0}, How can we help you?", user.username);
+
+
+            ConsoleKeyInfo c;
+            do
+            {
+
+                Console.WriteLine("Press 1 : Tax Code identify");
+                Console.WriteLine("Press 2 : Income Tax Caculation");
+                Console.WriteLine("Press 3 : See my inform");
+
+                ConsoleKeyInfo ms = Console.ReadKey();
+
+                Console.Clear();
+
+                switch (ms.Key)
+                {
+                    case ConsoleKey.D1:
+
+                        System_1 system_1 = new System_1();
+                        //user.Tax_Code = system_1.TaxCode_Classification();
+                        Console.WriteLine("Out of order");
+
+                        break;
+
+                    case ConsoleKey.D2:
+
+                        Console.WriteLine("Please enter your Income");
+                        System_2 system_2 = new System_2();
+                        double ic;
+                        if (double.TryParse(Console.ReadLine(), out ic))
+                        {
+                            if (ic >= 0)
+                            {
+                                user.Income = ic;
+                                user.Tax_Payable = system_2.Tax_Caculation(user.Income);
+                            }
+
+                            else
+                            {
+                                Console.WriteLine("Please enter the right value");
+                                break;
+                           
+                                
+                            }
+                         
+                        }
+                        else
+                        {
+                            Console.WriteLine("Please enter the numeric value");
+                            Console.Read();
+                            Console.Clear();
+                            goto case ConsoleKey.D2;
+                        }
+
+                        Console.WriteLine("Your tax payable is {0}", user.Tax_Payable);
+
+                        break;
+
+                    case ConsoleKey.D3:
+                        user.Show_Taxinformation();
+
+                        break;
+
+
+                    default:
+
+                        Console.WriteLine("You've been enter the wrong Key");
+                        Console.WriteLine("Please enter the right number");
+                        break;
+
+                }
+
+
+
+                Console.WriteLine("Wanna other services?");
+                Console.WriteLine("If you want, please press any key except the esc");
+                c = Console.ReadKey();
+                Console.Clear();
+
+
+            } while (c.Key != ConsoleKey.Escape);
+
+        }
+    }
+}
